195c699353b940fed10f1a7795c4b066d844bb26
Eduardo Toledo <eduardo_toledov@hotmail.com>
2020-09-11 18:40:25 -0500

    erge branch 'master' of https://github.com/toledov23/hyperblog
    Page Error

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import "./styles/PageError.css";
+
 function PageError(props) {
-  return props.error.message;
+  return <div className="PageError">{props.error.message}</div>;
 }
 
 export default PageError;
